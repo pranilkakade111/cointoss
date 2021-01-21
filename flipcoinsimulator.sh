a67ceb04fb0774430251e4260d69226226131462
Pranil Kakade <pranilkakade111@gmail.com>
2021-01-22 01:48:23 +0530

    Added:for flipping multiple coin

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -7,3 +7,20 @@ then
 else
     echo "it is tales"
 fi
+
+function coinFlip() 
+{
+rand=$(($RANDOM%2))
+hWin=0
+tWin=0
+if [ $rand -eq 0 ]
+then
+	echo "Heads Wins"
+	hwin=$(($hwin + 1))
+	echo $hwin
+else
+	echo "Tails Wins"
+	twin=$(($twin+1))
+	echo $twin
+fi
+}
